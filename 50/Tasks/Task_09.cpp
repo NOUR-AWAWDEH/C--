ab9e97d1ef96c7aaf11ca3c5659c0bc2957fe58a
NOUR-AWAWDEH <nour.awawdeh95@gmail.com>
2024-04-09 19:34:59 +0300

    modified:   50/Tasks/Task_09.cpp

--- 50/Tasks/Task_09.cpp
+++ 50/Tasks/Task_09.cpp
@@ -1,19 +1,33 @@
 #include<iostream>
 using namespace std;
 
+void ReadNumber(int &num1, int &num2, int &num3)
+{
+    cout << "Please Enter your Number 1 : " << endl;
+    cin >> num1;
+
+    cout << "Please Enter your Number 2 : " << endl;
+    cin >> num2;
+
+    cout << "Please Enter your Number 3 : " << endl;
+    cin >> num3;
+}
+
+int SumOf3Numbers(int num1,int num2, int num3)
+{
+    return num1 + num2 + num3;
+}
+
+void PrintResult(int total)
+{
+    cout << "\nThe Total Sum of Numbers is: " << total << endl;
+}
+
 int main()
 {
-    short Number1,Number2,Number3;
-    
-    cout <<"Please Enter Number1 : ";
-    cin >> Number1;
+    int Num1, Num2, Num3;
+    ReadNumber(Num1, Num2, Num3);
+    PrintResult(SumOf3Numbers(Num1, Num2, Num3));
 
-    cout <<"Please Enter Number2 : ";
-    cin >> Number2;
-
-    cout <<"Please Enter Number3 : ";
-    cin >> Number3;
-    short sum = Number1 + Number2 + Number3;
-    cout <<"The sum of the Numbers is "<< sum << endl; 
     return 0;
 }
