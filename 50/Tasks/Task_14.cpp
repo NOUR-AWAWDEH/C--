6534ff73d1a9d8832e9cf2a2e29fe85b85785a43
NOUR-AWAWDEH <nour.awawdeh95@gmail.com>
2024-04-10 12:37:32 +0300

    modified:   50/Tasks/Task_14.cpp

--- 50/Tasks/Task_14.cpp
+++ 50/Tasks/Task_14.cpp
@@ -10,25 +10,19 @@ void SwapNumbers(int& num1, int& num2)
 void PrintNumbers(int num1, int num2)
 {
     cout << "Number 1 : "<< num1 << endl;
-    cout << "Number 2 : "<< num2 << endl;
+    cout << "Number 2 : "<< num2 << endl << endl;
 }
 
 void ReadNumbers( int& num1, int& num2)
 {
     
-    cout << "Please Enter the Number 1: ";
+    cout << "Please Enter the Number 1 : ";
     cin >> num1;
     
-    cout << "Please Enter the Number 2: ";
+    cout << "Please Enter the Number 2 : ";
     cin >> num2;
 }
 
-void AfterSwapping(int Number1, int Number2)
-{
-    cout << "Numbers After Swapping : " << endl;
-    cout << Number1 << endl;
-    cout << Number2 << endl << endl;
-}
 
 int main()
 {
